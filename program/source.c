bf6ae27b986a180f1685c67bed1eb17ec2bb4f9e
hxun202 <kangsojin0918@gmail.com>
2025-12-17 21:43:53 +0900

    2025.12.17 조건문

--- program/source.c
+++ program/source.c
@@ -54,23 +54,139 @@
 
 #pragma region 
             // if문과 else if문의 조건이 다 틀리면 실행되는 명령문입니다.
-
-            int integer = 5;
-
-            if (integer < 5)
-            {
-                printf("5보다 작습니다.");
-            }
-            else if (integer > 5)
-            {
-                printf("5보다 큽니다.");
-            }
-            else
-            {
-                printf("5입니다.");
-            }
-
+            //
+            //int integer = 5;
+            //
+            //if (integer < 5)
+            //{
+            //    printf("5보다 작습니다.");
+            //}
+            //else if (integer > 5)
+            //{
+            //    printf("5보다 큽니다.");
+            //}
+            //else
+            //{
+            //    printf("5입니다.");
+            //}
+            //
             // if문에 연결된 모든 조건문의 조건이 맞을 때 가장 위에 있는 조건문만 실행됩니다.
+
+#pragma region switch문
+            // 어떤 결과에 따라 그 결과부터 실행되는 명령문입니다.
+            //
+           // char grade = 'K';
+            //
+            // switch문은 해당 조건이 완료되었을 때, break문이 없으면
+            // 나머지 밑에 있는 case문까지 계속 실행시키다가 종료합니다.
+            //
+          /*  switch (grade)
+            {
+            case 'A': printf("100~90점\n "); 
+                break;
+            case 'B': printf("89~80점\n ");
+                break;
+            case 'C': printf("79~70점\n ");
+                break;
+            case 'D': printf("69~60점\n ");
+                break;
+            case 'F': printf("60점 미만 \n");
+                break; 
+            default: printf("그 외\n");
+                break;
+            }*/
+          //
+            // switch문의 경우 조건에 해당하는 값에 따라 조건의 위치로 이동합니다.
+
+#pragma region 논리 연산자
+            
+#pragma region AND 연산자
+            // 두 개의 조건이 다 성립될 때 실행되는 연산자입니다.
+//int love = 60;
+//int hate = 30;
+//
+//if (love <=60 && hate >= 30)
+//{
+//    printf("사랑은 싫어하는 마음보다 크다");
+//}
+//
+// 조건문에서 하나 이상의 조건이 있다면 왼쪽부터 조건을 검사합니다.
+#pragma endregion
+
+#pragma region OR 연산자
+// 두 개의 조건 중에서 하나라도 조건이 성립될 때 실행되는 연산자입니다.
+
+//int score = 50;
+//int attitude = 70;
+//
+//if (score >= 50 || attitude > 70)
+//{
+//    printf("You are good");
+//}
+
+// 조건문의 논리 표현식을 평가하는 도중에 결과가 이미 확정이 났다면,
+// 그 이후의 평가는 생략합니다.
+
+#pragma endregion
+
+#pragma region NOT 연산자
+// 하나의 조건을 반전시키는 연산자입니다.
+
+//int power = 0;
+//
+//if (!power)
+//{
+//    printf("positive");
+//}
+//else
+//{
+//    printf("negative");
+//}
+
+#pragma endregion
+
+
+#pragma endregion
+
+#pragma region 사분면
+
+int x = 5;
+int y = 3;
+
+char x = 0;
+
+switch (x)
+{
+case 0: printf
+}
+if (x > 0 && y > 0)
+{
+    printf(" 제 1사분면에 도착했습니다.");
+}
+else if (x < 0 || y>0)
+{
+    printf("제 2사분면에 도착했습니다.");
+}
+else if (x < 0 && y < 0)
+{
+    printf("제 3사분면에 도착했습니다.");
+}
+else if (x > 0 || y<0)
+{
+    printf("제 4사분면에 도착했습니다.");
+}
+else if (x = 0 && y = 0)
+{
+    printf("이곳은 원점입니다.");
+}
+else if (x >= 0 || x <= 0, y=0)
+{
+    printf("x절편입니다.");
+}
+#pragma endregion
+
+
+
 
 
 
