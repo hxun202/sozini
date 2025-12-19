8f7f9b187963429e76414e17bb3a6a6feab345e1
hxun202 <kangsojin0918@gmail.com>
2025-12-19 21:42:26 +0900

    2025.12.19

---
+++ Class6th (Iteration Statement)/source.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+void main()
+{
+#pragma region  반복문
+
+#pragma endregion
+
+#pragma region 증감 연산자
+
+#pragma endregion
+
+#pragma region for문
+
+#pragma endregion
+
+#pragma region while문
+
+#pragma endregion
+
+#pragma region continue문
+    // 해당 조건문만 실행하지 않고, 반복문은 이어서 실행하는 제어문입니다.
+
+    //for (int i = 1;  i <= 5; i++)
+    //{
+    //    if (i == 3)
+    //    {
+    //        continue;
+    //    }
+    //    printf("%d", i);
+    //}
+#pragma endregion
+
+
+
+
+}
--- program/source.c
+++ program/source.c
@@ -1,196 +1,84 @@
 #include <stdio.h>
 
-        void main()
-    {
-#pragma region 조건문
-          // 어떤 조건이 주어질 때 해당 조건에 따라 동작을 수행하도록 실행하는 명령문입니다.
+#define SIZE 7
 
-#pragma region 관계 연산자
-            // 두 개의 피연산자의 값을 비교하여 그 결과를 0 또는 1이라는 값으로 나타내는 연산자이다.
-            //
-            //printf("10 > 20 : %d\n", 10 > 20);
-            //
-            //printf("10 < 20 : %d\n", 10 < 20);
-            //
-            //printf("15 >= 30 : %d\n", 15 >= 30);
-            //
-            //printf("25 <= 30 : %d\n", 25 <= 30);
-            //
-            //printf("10 != 10 : %d\n", 10 != 10);
-            //
-            //printf("10 == 10 : %d\n", 10 == 10);
-            //
-            // 관계 연산자는 조건이 맞을 때 1이라는 값으로 반환되며,
-            // 조건이 틀릴 때 0이라는 값으로 반환됩니다.
+void main()
+{
+#pragma region 자료형 변환
+	// 서로 다른 자료형을 가지고 있는 변수끼리 연산이 이루어 질 때
+	// 기존에 지정했던 자료형을 다른 자료형으로 변환하는 과정입니다.
 
-#pragma region if문
-            // 어떤 특정한 조건을 비교하며 조건이 맞다면 실행되는 명령문입니다.
-            //
-            //int health = 0;
-            //
-            //if (health<=0)
-            //{ 
-            //    printf("Destroy\n");
-            //
-            //}
-            //
-            // if문은 조건이 1일 때 실행되며, 0일 때 실행되지 않습니다.
+#pragma region 암묵적 형 변환
+	// 서로 다른 자료형으로 연산이 이루어질 때
+	// 자료형의 크기가 더 큰 자료형으로 변환되는 과정입니다.
 
-#pragma region else if문
-            // if문의 조건이 틀릴 때 else if문의 조건이 맞다면 실행되는 명령문입니다.
-            //
-           /* int level = 99;
+	// int defense = 10;
+	// float endurance = 3.5f;
 
-                if (level < 22)
-                {
-                    printf("You can't get in\n");
-                }
-                else if (level >= 99)
-                {
-                    printf("You can get in\n");
-                }*/
-            //
-                // else if문은 여러번 정의할 수 있으며, if문이 존재할 때 사용할 수 있습니다.
+	// printf("defense + endurance = %f\n", defense + endurance);
+	
 
-#pragma region 
-            // if문과 else if문의 조건이 다 틀리면 실행되는 명령문입니다.
-            //
-            //int integer = 5;
-            //
-            //if (integer < 5)
-            //{
-            //    printf("5보다 작습니다.");
-            //}
-            //else if (integer > 5)
-            //{
-            //    printf("5보다 큽니다.");
-            //}
-            //else
-            //{
-            //    printf("5입니다.");
-            //}
-            //
-            // if문에 연결된 모든 조건문의 조건이 맞을 때 가장 위에 있는 조건문만 실행됩니다.
-
-#pragma region switch문
-            // 어떤 결과에 따라 그 결과부터 실행되는 명령문입니다.
-            //
-           // char grade = 'K';
-            //
-            // switch문은 해당 조건이 완료되었을 때, break문이 없으면
-            // 나머지 밑에 있는 case문까지 계속 실행시키다가 종료합니다.
-            //
-          /*  switch (grade)
-            {
-            case 'A': printf("100~90점\n "); 
-                break;
-            case 'B': printf("89~80점\n ");
-                break;
-            case 'C': printf("79~70점\n ");
-                break;
-            case 'D': printf("69~60점\n ");
-                break;
-            case 'F': printf("60점 미만 \n");
-                break; 
-            default: printf("그 외\n");
-                break;
-            }*/
-          //
-            // switch문의 경우 조건에 해당하는 값에 따라 조건의 위치로 이동합니다.
-
-#pragma region 논리 연산자
-            
-#pragma region AND 연산자
-            // 두 개의 조건이 다 성립될 때 실행되는 연산자입니다.
-//int love = 60;
-//int hate = 30;
-//
-//if (love <=60 && hate >= 30)
-//{
-//    printf("사랑은 싫어하는 마음보다 크다");
-//}
-//
-// 조건문에서 하나 이상의 조건이 있다면 왼쪽부터 조건을 검사합니다.
+	// 표현 범위가 작은 데이터에 표현 범위가 큰 데이터를 저장하게 되면
+	// 암묵적으로 데이터 손실이 발생합니다.
 #pragma endregion
 
-#pragma region OR 연산자
-// 두 개의 조건 중에서 하나라도 조건이 성립될 때 실행되는 연산자입니다.
+#pragma region 명시적 형 변환
+	// 연산이 이루어지기 전에 사용자가 직접 자료형을 변환하는 과정입니다.
 
-//int score = 50;
-//int attitude = 70;
-//
-//if (score >= 50 || attitude > 70)
-//{
-//    printf("You are good");
-//}
+	// int critical = 10;
+	// int penetration = 3;
 
-// 조건문의 논리 표현식을 평가하는 도중에 결과가 이미 확정이 났다면,
-// 그 이후의 평가는 생략합니다.
+	// float attack = (float)critical / penetration;
+
+	//	printf("attack = %f", attack);
+
+   // 정수형 변수끼리 연산을 수행하게 되면 정수의 결과값만 가질 수 있습니다.
 
 #pragma endregion
 
-#pragma region NOT 연산자
-// 하나의 조건을 반전시키는 연산자입니다.
+#pragma region 단축 평가 계산
+	// 계산을 진행하는 도중에 결과가 미리 확정된 경우,
+	// 나머지 계산을 생략하는 과정입니다.
 
-//int power = 0;
-//
-//if (!power)
-//{
-//    printf("positive");
-//}
-//else
-//{
-//    printf("negative");
-//}
+	/*int x = 0;
+	int y = 0;
 
+	if (x != 0 && y++)
+	{
+		printf("short circuit");
+	}
+
+	printf("y의 값 : %d\n", y);
+
+	if (x == 0 || y++)
+	{
+		printf("first or operation\n");
+	}
+
+	if (x != 0 || y++)
+	{
+		printf("second or operation\n");
+	}
+
+	printf("y의 값 : %d\n", y);*/
+
+#pragma endregion
+
+#pragma region 매크로
+	// 프로그램 내에서 특정한 데이터가 문자열로 정의되고 처리되는 과정입니다.
+
+	// printf("매크로 SIZE의 값 : %d\n", SIZE);
+
+	// 매크로의 경우 자료형이 존재하지 않으므로 메모리 공간을 가지고 있지 않습니다.
+
+	// ex) SIZE = 100;
+
+	// 매크로의 과정은 컴파일 이전에 실행되며, 각 매크로가 실행될 때
+	// 매크로의 대체 목록을 넣어야 하므로 프로그램의 크기가 커지게 됩니다.
 #pragma endregion
 
 
 #pragma endregion
-
-#pragma region 사분면
-
-int x = 5;
-int y = 3;
-
-char x = 0;
-
-switch (x)
-{
-case 0: printf
-}
-if (x > 0 && y > 0)
-{
-    printf(" 제 1사분면에 도착했습니다.");
-}
-else if (x < 0 || y>0)
-{
-    printf("제 2사분면에 도착했습니다.");
-}
-else if (x < 0 && y < 0)
-{
-    printf("제 3사분면에 도착했습니다.");
-}
-else if (x > 0 || y<0)
-{
-    printf("제 4사분면에 도착했습니다.");
-}
-else if (x = 0 && y = 0)
-{
-    printf("이곳은 원점입니다.");
-}
-else if (x >= 0 || x <= 0, y=0)
-{
-    printf("x절편입니다.");
-}
-#pragma endregion
-
-
-
-
-
-
-
 
 
 }
