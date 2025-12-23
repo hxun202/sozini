549150f3be61af336287e401accddfbf9965ae17
hxun202 <kangsojin0918@gmail.com>
2025-12-23 21:44:44 +0900

    2025.12.23 범용 포인터

--- program/source.c
+++ program/source.c
@@ -37,28 +37,84 @@ void main()
 #pragma region 포인터
 	// 메모리의 주소 값을 저장할 수 있는 변수입니다.
 
-	int storage = 10;
-	int *pointer = &storage;
+	//int storage = 10;
+	//int *pointer = &storage;
 
 	// 포인터가 가리키는 메모리 공간의 자료형은 알 수 없으므로,
 	// 포인터가 가리키는 메모리의 자료형을 선언해주어야 합니다.
 
-	printf("storage의 주소 값 : %p\n", &storage);
-	printf("pointer의 값 : %p\n", pointer);
-    printf("pointer의 주소 : %p\n", &pointer);
+	//printf("storage의 주소 값 : %p\n", &storage);
+	//printf("pointer의 값 : %p\n", pointer);
+ //   printf("pointer의 주소 : %p\n", &pointer);
 
-	*pointer = 99;
+	//*pointer = 99;
 
-	printf("storage의 값 : %d\n", storage);
-	printf("pointer 변수가 가리키는 값 : %d\n", *pointer);
+	//printf("storage의 값 : %d\n", storage);
+	//printf("pointer 변수가 가리키는 값 : %d\n\n", *pointer);
 
-	int room = 20;
+	//int room = 20;
+	//pointer = &room;
 
-	printf("room의 값 : %d\n", room);
-	printf("pointer 변수가 가리키는 값 : %p\n", &storage);
+	//printf("pointer의 값 : %p\n", pointer);
+	//printf("pointer 변수가 가리키는 값 : %d\n\n", *pointer);
+
+	// 포인터 변수도 자신의 메모리 공간을 가지고 있으며,
+	// 포인터 변수에 변수의 주소를 저장하게 되면 해당 변수의 시작 주소를 가리킵니다.
+
+	//printf("int의 크기는 : %u\n\n", sizeof(int));
+	//printf("pointer 변수의 크기 : %u\n", sizeof(pointer));
+
+	// 포인터 변수의 크기는 중앙 처리 장치가 한 번에 처리할 수 있는
+	// 크기로 정해지며, 한 번에 처리할 수 있는 크기는 운영체제에 따라 크기가 결정됩니다.
 
 #pragma endregion
 
+#pragma region 범용 포인터
+	// 자료형이 정해지지 않은 상태로 모든 자료형을 저장할 수 있는 포인터입니다.
+
+	//char rank = 'A';
+	//int count = 10;
+	//float angle = 35.75f;
+
+	//void* reference = NULL;
+
+	//reference = &rank;
+
+	// 범용  포인터는 메모리 주소에 접근해서 값을 변경할 수 업습니다.
+
+	//*(char*)reference = 'B';
+
+	//printf("rank의 크기 : %c\n", rank);
+	//printf("reference의 크기 : %p\n\n", reference);
+
+	//reference = &count;
+	//*(int*)reference = 50;
+
+	//printf("count의 크기 : %d\n", count);
+	//printf("reference의 크기 : %p\n\n", reference);
+
+	//reference = &angle;
+	//*(float*)reference = 8.125f;
+
+	//printf("angle의 크기 : %f\n", angle);
+	//printf("reference의 크기 : %p\n\n", reference);
+
+	// 범용 포인터로 변수의 메모리에 접근하려면 범용 포인터가
+	// 가리키는 변수의 자료형으로 변환해주어야 합니다.
+#pragma endregion
+
+#pragma region 상수 지시 포인터
+// 포인터가 가리키는 주소에 저장되어 있는 값을 변경할 수 없도록
+// 지정되어 있는 포인터 입니다. 
+
+int z = 20;
+int y = 10;
+
+const int* fixedpointer = &y;
+
+
+
+#pragma endregion
 
 
 }
