306287bd2ae8117c2aa1010ea1d456d4aa7378f6
hxun202 <kangsojin0918@gmail.com>
2026-01-05 21:44:03 +0900

    2026.1.5 구조체

---
+++ Class12th (Dynamic Allocation)/source.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void main()
+{
+#pragma region 동적 할당
+	// 프로그램을 실행하는 중에 필요한 만큼 메모리를 할당하는 작업입니다.
+
+	// int *pointer = malloc(sizeof(int));
+
+	//*pointer = 99;
+
+	// printf("pointer 변수가 가리키는 값 : %d\n", *pointer);
+
+	//// 동적 할당은 실행 시간에 가변적으로 메모리의 크기를
+	//// 변경시킬 수 있으며, 동적으로 메모리의 크기를 할당할 때 
+	//// 바이트 단위로 지정합니다.
+	//
+	//free(pointer);
+
+	//// 동적으로 할당한 메모리는 힙 영역에 보관되어 있으므로,
+	//// 사용이 끝나면 직접 해제해주어야 합니다.
+
+	//pointer = malloc(sizeof(int)*3);
+
+	//*pointer = 10;
+
+	//for (int i = 0; i < 3; i++)
+	//{
+	//	pointer[i] = (1 + i) * 10;
+
+	//	printf("pointer[%d] = %d\n", i, pointer[i]);
+	//}
+
+	//free(pointer);
+#pragma endregion
+
+
+}
